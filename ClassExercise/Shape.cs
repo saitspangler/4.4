4c5e276c3747693978e15fd083e6049c5c048580
saitspangler <peter.spangler@edu.sait.ca>
2023-04-04 14:40:15 -0600

    Add project files.

---
+++ ClassExercise/Shape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExercise
+{
+    public class Shape
+    {
+        string id;
+        double CalculateArea()
+        {
+            return 0;
+        }
+    }
+}
